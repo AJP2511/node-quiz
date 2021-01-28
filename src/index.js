ad36bdb945e247218576a6994e3a17b08db01d14
AJP2511 <johnson2511@gmail.com>
2021-01-28 14:57:39 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const routes = require("./Routes");
 const mongoose = require("mongoose");
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://radioquiz.ajp2511.vercel.app"
+  app.use(
+    cors({
+      origin: "https://radioquiz.ajp2511.vercel.app",
+      methods: "GET, POST",
+      optionsSuccessStatus: 200,
+    })
   );
-  app.use(cors());
   next();
 });
 
