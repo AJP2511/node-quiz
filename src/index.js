04d06cfba0a279a7d4bb47f88381d6127f1a21fe
AJP2511 <johnson2511@gmail.com>
2021-01-28 14:52:25 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-require("dotenv").config();
 const { urlencoded } = require("express");
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 const routes = require("./Routes");
 const mongoose = require("mongoose");
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://radioquiz.ajp2511.vercel.app/"
+  );
+  app.use(cors());
+  next();
+});
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -12,7 +20,6 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.use(urlencoded({ extended: true }));
-app.use(cors({}));
 app.use(express.json());
 app.use(routes);
 
