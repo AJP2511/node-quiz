2030763e1bd964c0e2bdac116d07ba9ae8d917f1
AJP2511 <johnson2511@gmail.com>
2021-01-28 12:49:12 -0300

    replace

--- src/Routes.js
+++ src/Routes.js
@@ -10,10 +10,19 @@ route.get("/", async (req, res) => {
 route.post("/", async (req, res) => {
   const { nome, pontos } = req.body;
 
-  const user = await Usuario.create({
-    nome,
-    pontos,
-  });
+  const pegaUsuario = await Usuario.find({ nome });
+
+  if (pegaUsuario) {
+    const usuarioUpdate = await Usuario.findOneAndUpdate(
+      { nome: nome },
+      { pontos: pontos }
+    );
+  } else {
+    const user = await Usuario.create({
+      nome,
+      pontos,
+    });
+  }
 
   return res.json(user);
 });
