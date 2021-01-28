054f3d4282b32e1f43b5adfb7cf86a6c01e0002e
AJP2511 <johnson2511@gmail.com>
2021-01-28 07:52:37 -0300

    .

--- src/SchemaPlayer.js
+++ src/SchemaPlayer.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   nome: String,
   pontos: Number,
-  expireAfterSeconds: 60,
 });
 
 module.exports = mongoose.model("Usuario", UserSchema);
