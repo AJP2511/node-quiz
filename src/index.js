272bb0273ace870d5e204dfe65d9e49d39af7f0c
AJP2511 <johnson2511@gmail.com>
2021-01-28 15:04:51 -0300

    .

--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ const app = express();
 const routes = require("./Routes");
 const mongoose = require("mongoose");
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "https://radioquiz.ajp2511.vercel.app/quiz",
-    "https://radioquiz.ajp2511.vercel.app",
-  ]);
-  app.use(cors());
-  next();
-});
-
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+app.use(cors({}));
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
