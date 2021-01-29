12ff897dda347d7b9cac4da0695699a35551002d
AJP2511 <johnson2511@gmail.com>
2021-01-29 12:57:07 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const routes = require("./Routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/quiz", {
   useNewUrlParser: true,
