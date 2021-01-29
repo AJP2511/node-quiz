87bd489e1f6463d0aeac25f9866689210f5523c1
AJP2511 <johnson2511@gmail.com>
2021-01-29 12:37:29 -0300

    cors change place

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const app = express();
 const routes = require("./Routes");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-app.use(cors({}));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/quiz", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("conexão com o banco de dados estabelecida");
+  });
+
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
