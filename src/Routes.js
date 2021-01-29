a2092794de5974a2265b9d5cebeb2dd930cca345
AJP2511 <johnson2511@gmail.com>
2021-01-29 12:47:46 -0300

    .

--- src/Routes.js
+++ src/Routes.js
@@ -2,20 +2,13 @@ const route = require("express").Router();
 const Usuario = require("./SchemaPlayer");
 const cors = require("cors");
 
-route.get(
-  "/",
-  cors({
-    origin: "https://radioquiz.ajp2511.vercel.app",
-    optionsSuccessStatus: 200,
-  }),
-  async (req, res) => {
-    const user = await Usuario.find({ pontos: { $gt: 40 } }).sort({
-      pontos: -1,
-    });
+route.get("/", cors(), async (req, res) => {
+  const user = await Usuario.find({ pontos: { $gt: 40 } }).sort({
+    pontos: -1,
+  });
 
-    return res.json(user);
-  }
-);
+  return res.json(user);
+});
 
 route.post(
   "/",
